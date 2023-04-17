5028ccab6d19dc7e5aa6a669afc2ffebbd32c01a
Alexandre ZANNI <alexandre@acceis.fr>
2023-04-17 16:29:42 +0200

    better comments

--- access-control/app.fix.js
+++ access-control/app.fix.js
@@ -3,10 +3,13 @@ const app = express();
 const auth = require('./authentication');
 const secret = require('./secret');
 
-app.set('case sensitive routing', true); // cf. https://expressjs.com/en/api.html#app.settings.table
+// cf. https://expressjs.com/en/api.html#app.settings.table
+app.set('case sensitive routing', true);
 
 app.use((req, res, next) => {
-  if (req.url.startsWith('/secret')) { // or case incentive check /^\/secret/i.test(req.url)
+  // or case incentive check: /^\/secret/i.test(req.url)
+  // Regexp is probably not the right way to handle it
+  if (req.url.startsWith('/secret')) {
     const authorized = auth.verify(req);
     if (!authorized) {
       return res.status(401).send('Incorrect authentication token!');
