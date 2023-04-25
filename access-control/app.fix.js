662e72a520918d3c2ec1e5c70e50a064c3e306aa
Alexandre ZANNI <alexandre@acceis.fr>
2023-04-25 12:31:55 +0000

    fix typo

--- access-control/app.fix.js
+++ access-control/app.fix.js
@@ -7,7 +7,7 @@ const secret = require('./secret');
 app.set('case sensitive routing', true);
 
 app.use((req, res, next) => {
-  // or case incentive check: /^\/secret/i.test(req.url)
+  // or case insensitive check: /^\/secret/i.test(req.url)
   // Regexp is probably not the right way to handle it
   if (req.url.startsWith('/secret')) {
     const authorized = auth.verify(req);
