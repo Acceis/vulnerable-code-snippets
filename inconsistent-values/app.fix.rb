ced029663fe27951ab5c18789fb775d0414b052e
Alexandre ZANNI <alexandre@acceis.fr>
2023-05-09 17:25:20 +0200

    protects against TOCTOU

--- inconsistent-values/app.fix.rb
+++ inconsistent-values/app.fix.rb
@@ -7,8 +7,9 @@ class App < Roda
     r.get 'local' do
       addr = r.ip # safer than parsing HTTP headers
       addr = "http://#{addr}"
-      if URI.parse(addr).host == '127.0.0.1'
-        safe_addr = URI.parse(addr) # always use the same method to process the data that was used in the security check
+      parsed_addr = URI.parse(addr) # process only one time ad store to avoid TOCTOU attacks
+      if parsed_addr.host == '127.0.0.1'
+        safe_addr = parsed_addr.dup # always use the same method to process the data that was used in the security check
         safe_addr.path = '/login'
         data = {user: 'admin', pass: 'AJMMbzLckY37'}
         begin
